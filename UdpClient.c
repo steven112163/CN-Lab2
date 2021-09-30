4c722099806cc777a088b5aac618aca27ff4676a
steven112163 <steven112163@gmail.com>
2021-10-01 01:09:33 +0800

    Implement udp add server-client

--- UdpClient.c
+++ UdpClient.c
@@ -10,27 +10,22 @@
 #define PORT 8000
 
 int main(void) {
-    struct sockaddr_in client;
-    int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    struct sockaddr_in server;
+    socklen_t server_len = sizeof(server);
+    int socket_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     if (socket_fd == -1) {
         perror("Socket creation failed\n");
         exit(EXIT_FAILURE);
     }
 
-    memset(&client, 0, sizeof(struct sockaddr_in));
+    memset(&server, 0, sizeof(struct sockaddr_in));
 
-    client.sin_family = AF_INET;
-    client.sin_port = htons(PORT);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, ADDRESS, &client.sin_addr) == 0) {
+    if (inet_pton(AF_INET, ADDRESS, &server.sin_addr) == 0) {
         perror("Address is invalid\n");
-        close(socket_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (connect(socket_fd, (struct sockaddr *) &client, sizeof(struct sockaddr_in)) == -1) {
-        perror("Connection failed\n");
         close(socket_fd);
         exit(EXIT_FAILURE);
     }
@@ -42,17 +37,20 @@ int main(void) {
         char question[21];
         scanf("%s", question);
 
-        if (send(socket_fd, question, 21, 0) < 0) {
+        if (strcmp(question, exit_string) == 0)
+            break;
+
+        if (sendto(socket_fd, question, 21, 0, (struct sockaddr *) &server, server_len) < 0) {
             perror("Client write failed\n");
             close(socket_fd);
             exit(EXIT_FAILURE);
         }
 
-        if (strcmp(question, exit_string) == 0 || strcmp(question, close_string) == 0)
+        if (strcmp(question, close_string) == 0)
             break;
 
         char result[10];
-        int read = recv(socket_fd, result, 10, 0);
+        int read = recvfrom(socket_fd, result, 10, 0, (struct sockaddr *) &server, &server_len);
         if (read < 0) {
             perror("Client read failed\n");
             close(socket_fd);
@@ -66,4 +64,3 @@ int main(void) {
 
     return 0;
 }
-
---
+++ UdpServer.c
@@ -0,0 +1,83 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 8000
+#define BUFFER_SIZE 21
+
+int main(void) {
+    struct sockaddr_in server, client;
+    socklen_t client_len = sizeof(client);
+    int socket_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (socket_fd == -1) {
+        perror("Socket creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server, 0, sizeof(struct sockaddr_in));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8000);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(socket_fd, (struct sockaddr *) &server, sizeof(server)) == -1) {
+        perror("Socket binding failed\n");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFFER_SIZE];
+    char exit_string[] = "exit";
+    char close_string[] = "close";
+    printf("Server listening on port %d\n", PORT);
+    while (1) {
+        int receive_fd = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &client, &client_len);
+        char client_ip[20];
+        strcpy(client_ip, inet_ntoa(client.sin_addr));
+        printf("Packet from: %s\n", client_ip);
+
+        if (receive_fd < 0) {
+            perror("Receiving the packet failed\n");
+            close(receive_fd);
+            close(socket_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (strcmp(buffer, exit_string) == 0) {
+            shutdown(receive_fd, SHUT_RDWR);
+            close(receive_fd);
+            continue;
+        }
+
+        if (strcmp(buffer, close_string) == 0) {
+            shutdown(receive_fd, SHUT_RDWR);
+            close(receive_fd);
+            break;
+        }
+
+        int first, second;
+        sscanf(buffer, "%d+%d", &first, &second);
+        char result[10];
+        sprintf(result, "%d", first + second);
+        printf("Result: %d + %d = %s\n", first, second, result);
+
+        if (sendto(socket_fd, result, 10, 0, (struct sockaddr *) &client, client_len) < 0) {
+            perror("Client write failed\n");
+            close(receive_fd);
+            close(socket_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        shutdown(receive_fd, SHUT_RDWR);
+        close(receive_fd);
+    }
+
+    close(socket_fd);
+    printf("Server closed\n");
+    return 0;
+}
